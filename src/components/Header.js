1f0c38f86138fd83de6556952c532adf1d02d095
rahulr <rahulr@qburst.com>
2017-09-13 11:04:05 +0530

    Updates

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import {Navbar} from 'react-materialize'
-
-class Header extends Component {
-  render() {
-    return <Navbar center>Shopping Cart!</Navbar>
-  };
-}
-export default Header
--- src/reducers/filter.js
+++
@@ -1,8 +0,0 @@
-export default function(state = {min:0,max:100000}, action) {
-  if (action.type === 'FILTER') {
-    let newState = Object.assign({},state,action.payload)
-console.log(newState)
-    return newState;
-  }
-  return state;
-}
